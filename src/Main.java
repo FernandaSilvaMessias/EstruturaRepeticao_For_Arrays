facfa195c668942ab07dda7ce29c77ed163bde5f
CassiaFernandaMessias <cassia.fernanda.dev@gmail.com>
2024-06-09 08:53:27 -0300

    ForArrays

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String alunos []= {"Felipe", "Jonas","Júlia","Marcos" };
+        for( int x =0; x<alunos.length; x++){
+        System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+    }
+}}
